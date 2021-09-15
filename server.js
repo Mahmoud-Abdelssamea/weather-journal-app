6872ff6e0c4e1f811611e42f39b4aaf3be93c807
mahmoud-awwad <mabdelssamea@gmail.com>
2021-09-15 23:39:22 +0300

    make projectData as endpoint api

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const port = 3000;
 //create an instance of express and name it app
 const app = express();
 
+// create an object
+const projectData = {};
+const key = "0d9003ce99540f980a4ba3acc599c020";
+
 //use cors backage
 app.use(cors());
 
@@ -14,7 +18,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // middleware
 // build a get request
 app.get("/", (req, res) => {
-  res.send("hello");
+  //  projectData as endpoint API
+  res.send(projectData);
 });
 
 // add a host to start  runnning the server
