001231ee64e7970886b34e3a333cf96421fcd512
mahmoud-awwad <mabdelssamea@gmail.com>
2021-09-15 23:05:38 +0300

    add a host to the server and run it

--- server.js
+++ server.js
@@ -2,9 +2,14 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
+const port = 3000;
 //create an instance of express and name it app
 const app = express();
 
 //use cors backage
 app.use(cors());
+
+// add a host to start  runnning the server
+app.listen(port, () => {
+  console.log(`SERVER IS RUNNING AT PORT ${port}`);
+});
