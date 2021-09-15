837395fc8c8c50ba7c947cecdad8409a5c16876f
mahmoud-awwad <mabdelssamea@gmail.com>
2021-09-15 22:59:04 +0300

    create an instance of express backage and name it app and use cors

--- server.js
+++ server.js
@@ -2,3 +2,9 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+
+//create an instance of express and name it app
+const app = express();
+
+//use cors backage
+app.use(cors());
