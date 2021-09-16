24f3ada4ff9e76971d0ef2559b7faab0a00b55e1
mahmoud-awwad <mabdelssamea@gmail.com>
2021-09-16 11:56:07 +0300

    get all elements required from html and name it

--- website/app.js
+++ website/app.js
@@ -1,5 +1,9 @@
-/* Global Variables */
-
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+// Start Global variables
+const zipcode = document.getElementById("zip");
+const feeling = document.getElementById("feelings");
+const generateBtn = document.getElementById("generate");
+// End Global variables
